8df86c609373cfaee15df81da58564787ca79814
aryan <w0848380@myscc.ca>
2024-09-18 11:08:02 -0400

    done with all steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Counter } from './Shared/Models/Counter';
+import { JsonPipe, NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf, JsonPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Aryan-Parmar-Learning-Angular';
-  about = 'Aryan is topper';
-  heading = 'My name is aryan parmar';
+
+  Counter1: Counter = { id: 1, playerName: "Aryan", guns: "Ak47", places: "Windsor", isCars: true };
+  Counter2: Counter = { id: 2, playerName: "Akal", guns: "Uzzi", places: "Windsor", isCars: false };
+  Counter3: Counter = { id: 3, playerName: "Dinesh", guns: "UMP", places: "Windsor", isCars: true };
+  Counter4: Counter = { id: 4, playerName: "Mayank", guns: "M24", places: "Windsor", isCars: false };
+  Counter5: Counter = { id: 5, playerName: "Muskan", guns: "M4", places: "Windsor", isCars: false };
+  Counter6: Counter = { id: 6, playerName: "Anil", guns: "Mk16", places: "Windsor", isCars: true };
+
+  counterList: Counter[] = [this.Counter1, this.Counter2, this.Counter3, this.Counter4, this.Counter5, this.Counter6];
 }
