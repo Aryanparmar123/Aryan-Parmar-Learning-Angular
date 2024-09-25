cccbba77c230b938a4994735bbd91b8a52675462
aryan <w0848380@myscc.ca>
2024-09-24 22:43:31 -0400

    made new components files

---
+++ src/app/event-list-item/event-list-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './event-list-item.component.html',
+  styleUrl: './event-list-item.component.css'
+})
+export class EventListItemComponent {
+
+}
