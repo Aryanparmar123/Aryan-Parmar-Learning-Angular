8d80a244c6df2e00a28f1e6449f9fd4beb72ba66
Aryan Parmar <w0848380@myscc.ca>
2024-10-08 20:10:51 -0400

    did step 4 5 made service and mockcontent

---
+++ src/app/data/mock-content.ts
@@ -0,0 +1,28 @@
+import { events } from '../Shared/Models/events';
+
+export const eventlist: events[] = [
+  {
+    eventId:1, Name: "Music Festival", place: "Central Park", type: "Concert", cordinator:"joseph",
+    description: "An outdoor music festival featuring various artists."
+  },
+  {
+    eventId:2, Name: "Art Exhibition", place: "City Gallery", type: "Exhibition", cordinator:"joseph",
+    description: "A showcase of contemporary art from local artists."
+  },
+  {
+    eventId:3, Name: "Tech Conference", place: "Convention Center", type: "Conference", cordinator:"joseph",
+    description: "A gathering of tech enthusiasts and professionals."
+  },
+  {
+    eventId:4, Name: "Food Fair", place: "Downtown Square", type: "Fair", cordinator:"joseph",
+    description: "A celebration of local cuisine and culinary delights."
+  },
+  {
+    eventId:5,
+    Name: "Theater Play", place: "Main Theater", type: "Performance",cordinator:"joseph",
+    description: "A dramatic play performed by the local theater group."
+  },
+  {
+    eventId:6, Name: "Charity Run", place: "City Park", type: "Sporting Event", cordinator:"joseph",
+    description: "A fun run to raise funds for local charities."}
+]
